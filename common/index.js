3822c417597a952ef44b2658b3fab15059161109
Sarah Kwak <sarahkwak79@gmail.com>
2024-04-27 04:50:58 -0400

    first commit

---
+++ common/index.js
@@ -0,0 +1,14 @@
+const Yup = require("yup");
+
+const formSchema = Yup.object({
+  username: Yup.string()
+    .required("Username required!")
+    .min(6, "Username is too short!")
+    .max(28, "Username is too long!"),
+  password: Yup.string()
+    .required("Password required!")
+    .min(6, "Password is too short!")
+    .max(28, "Password is too long!"),
+});
+
+module.exports = { formSchema };
---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const authRouter = require("./routers/authRouter");
+
+const server = require("http").createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: "true",
+  },
+});
+
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
+
+io.on("connect", socket => {});
+
+server.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
